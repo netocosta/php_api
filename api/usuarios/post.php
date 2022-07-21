8ee9d71b8ad41c1645e078f1c1d9933b31009949
Neto Costa <54679690+netocosta@users.noreply.github.com>
2022-07-20 21:08:53 -0300

    Adicionado login

---
+++ api/usuarios/post.php
@@ -0,0 +1,10 @@
+<?php
+
+if ($acao == '' && $param == '') {
+    echo json_encode(['ERRO' => 'Caminho não encontrado!']); exit; 
+}
+
+if ($acao == 'login' && $param == '') {
+    Usuarios::login($_POST['login'], $_POST['senha']);
+    exit;
+}
--- api/usuarios/usuarios.php
+++ api/usuarios/usuarios.php
@@ -1,1 +1,7 @@
+<?php
 
+if ($api == 'usuarios') {
+    if ($method == "POST" && !isset($_POST['_method'])) {
+        include_once "post.php";
+    }
+}
