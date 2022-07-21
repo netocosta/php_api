42a9ee8df61b40d70691eb3f8b3fa638732fda5c
Neto Costa <54679690+netocosta@users.noreply.github.com>
2022-07-20 21:08:08 -0300

    Adicionado login

--- api/clientes/clientes.php
+++ api/clientes/clientes.php
@@ -2,20 +2,24 @@
 
 if ($api == 'clientes') {
 
-    if ($method == "GET") {
-        include_once "get.php";
-    }
+    if (Usuarios::verificar()) {
+        if ($method == "GET") {
+            include_once "get.php";
+        }
+    
+        if ($method == "POST" && !isset($_POST['_method'])) {
+            include_once "post.php";
+        }
 
-    if ($method == "POST" && !isset($_POST['_method'])) {
-        include_once "post.php";
-    }
+        if ($method == "POST" && isset($_POST['_method']) && $_POST['_method'] == "PUT") {
+            include_once "put.php";
+        }
 
-    if ($method == "POST" && isset($_POST['_method']) && $_POST['_method'] == "PUT") {
-        include_once "put.php";
-    }
-
-    if ($method == "POST" && isset($_POST['_method']) && $_POST['_method'] == "DELETE") {
-        include_once "delete.php";
+        if ($method == "POST" && isset($_POST['_method']) && $_POST['_method'] == "DELETE") {
+            include_once "delete.php";
+        }
+    } else {
+        echo json_encode(['ERRO' => 'Você não está logado, ou seu token é inválido.']);
     }
 
 }
