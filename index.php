67345d79fe08db978f73eb0911e723c3338d8c81
Neto Costa <54679690+netocosta@users.noreply.github.com>
2022-08-18 20:03:45 -0300

    Adicionado sistema de rotas

--- index.php
+++ index.php
@@ -4,19 +4,18 @@ header('Content-type: application/json');
 
 date_default_timezone_set("America/Sao_Paulo");
 
-if (isset($_GET['path'])) {
-    $path = explode("/", $_GET['path']);
-} else { 
-    echo "Caminho não existe"; exit;
-}
-
-if (isset($path[0])) { $api = $path[0]; } else { echo "Caminho não existe"; exit; }
-if (isset($path[1])) { $acao = $path[1]; } else { $acao = ''; }
-if (isset($path[2])) { $param = $path[2]; } else { $param = ''; }
-
-$method = $_SERVER['REQUEST_METHOD'];
-
 $GLOBALS['secretJWT'] = '123456';
+
+# Autoload
 
 include_once "classes/autoload.class.php";
 new Autoload();
+
+# Rotas
+
+$rota = new Rotas();
+$rota->add('POST', '/usuarios/login', 'Usuarios::login', false);
+$rota->add('GET', '/clientes/listar', 'Clientes::listarTodos', true);
+$rota->add('GET', '/clientes/listar/[PARAM]', 'Clientes::listarUnico', true);
+$rota->add('PUT', '/clientes/atualizar/[PARAM]', 'Clientes::atualizar', true);
+$rota->ir($_GET['path']);
